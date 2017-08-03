2222b0b488e6c123e46ba37cd323a48fc55ac41c
Simon Reynolds <simonreyno@gmail.com>
2017-08-03 12:30:21 +0100

    First commit :)

--- Oppositify CLR/ViewController.swift
+++ Oppositify CLR/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.white
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var CLR: UIView!
+    
+    @IBAction func changered(_ sender: UISlider) {
 
+    }
 }
-
