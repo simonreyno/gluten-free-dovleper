b51f5d2e315c7fd783dd7051de0c2a966e461389
Simon Reynolds <simonreyno@gmail.com>
2017-10-12 17:25:26 +0100

    Coffee, beer, make the sliders display their output values, create a variable from those outputs, and turn them into RGB values for two colours, one the "opposite" of the other - need to make it a bit prettier, fix the width, margins etc

--- Oppositify CLR/ViewController.swift
+++ Oppositify CLR/ViewController.swift
@@ -15,18 +15,41 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.white
         
-        chosenColourView.backgroundColor = .orange
-       oppositifiedColourView.backgroundColor = .cyan
+        chosenColourView.backgroundColor = .black
+        oppositifiedColourView.backgroundColor = .white
         
     }
     
-
-
+    
+    
     @IBOutlet weak var chosenColourView: UIView!
     @IBOutlet weak var oppositifiedColourView: UIView!
     
-    @IBAction func changered(_ sender: UISlider) {
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBAction func changeRed(_ sender: UISlider) {
+        print("Red \(sender.value)")
+        updateColour()
+    }
+    @IBAction func changeGreen(_ sender: UISlider) {
+        print("Green \(sender.value)")
+        updateColour()
 
-        print(sender.value)
+    }
+    @IBAction func changeBlue(_ sender: UISlider) {
+        print("Blue \(sender.value)")
+        updateColour()
+
+    }
+    func updateColour() {
+        let r = redSlider.value
+        let g = greenSlider.value
+        let b = blueSlider.value
+        let colour = UIColor(red: CGFloat(r / 100), green: CGFloat(g / 100), blue: CGFloat(b / 100), alpha: 1)
+        chosenColourView.backgroundColor = colour
+        let oppositeColour = UIColor(red: CGFloat( 1 - r / 100), green: CGFloat(1 - g / 100), blue: CGFloat(1 - b / 100), alpha: 1)
+        oppositifiedColourView.backgroundColor = oppositeColour
     }
 }
